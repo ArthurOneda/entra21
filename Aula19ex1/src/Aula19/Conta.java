82e3b68fa702cb1afeb14bcfd2065292b9f85fca
Arthur Valentim Oneda <143853430+ArthurOneda@users.noreply.github.com>
2023-10-23 18:21:41 -0300

    Add files via upload

---
+++ Aula19ex1/src/Aula19/Conta.java
@@ -0,0 +1,7 @@
+package Aula19;
+
+public class Conta {
+	double saldo = 0;
+	String proprietário;
+	int conta;
+}
---
+++ Aula19ex1/src/Aula19/exercicio1.java
@@ -0,0 +1,10 @@
+package Aula19;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		Conta c = new Conta();
+		
+	}
+
+}
