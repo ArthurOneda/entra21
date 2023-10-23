2d0b1c628fe7781aa95081e383adb469e2674edb
Arthur Valentim Oneda <143853430+ArthurOneda@users.noreply.github.com>
2023-10-23 18:16:48 -0300

    Add files via upload

---
+++ Aula18ex1/src/Aula18/Pessoa.java
@@ -0,0 +1,8 @@
+package Aula18;
+
+public class Pessoa {
+	String nome;
+	char sexo;
+	String data;
+	String estadoC;
+}
---
+++ Aula18ex1/src/Aula18/exercicio1.java
@@ -0,0 +1,18 @@
+package Aula18;
+
+public class exercicio1 {
+	public static void main(String[] args) {
+		Pessoa pe1 = new Pessoa();
+		Pessoa pe2 = new Pessoa();
+		pe1.nome = "Arthur";
+		pe1.sexo = 'M';
+		pe1.data = "07/03/2006";
+		pe1.estadoC = "Solteiro";
+		
+		pe2.nome = "Gui";
+		pe2.sexo = 'M';
+		pe2.data = "09/09/20003";
+		pe2.estadoC = "Cabaço";
+	}
+
+}
