919d98bd9c272ad7111a1c5004ccc9751e724cb3
Arthur Valentim Oneda <143853430+ArthurOneda@users.noreply.github.com>
2023-10-23 18:22:30 -0300

    Add files via upload

---
+++ Aula20ex6/src/Aula20/Cidade.java
@@ -0,0 +1,9 @@
+package Aula20;
+
+public class Cidade {
+	String nome;
+	String uf;
+	int populacaoMasculina;
+	int populacaoFeminina;
+	boolean calculado;
+}
---
+++ Aula20ex6/src/Aula20/exercicio6.java
@@ -0,0 +1,18 @@
+package Aula20;
+
+public class exercicio6 {
+
+	public static void main(String[] args) {
+		Cidade gaspar = new Cidade();
+		gaspar.nome = ("Gaspar");
+		gaspar.uf = ("SC");
+		gaspar.populacaoMasculina = (28141);
+		 
+		Cidade indaial = new Cidade();
+		indaial.nome = ("Indaial");
+		indaial.populacaoFeminina = (25844);
+		indaial.calculado = (true);
+
+	}
+
+}
